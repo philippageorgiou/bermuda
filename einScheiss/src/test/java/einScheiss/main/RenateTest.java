cf9cd56f1d4d791fda887c297b7f33d3eaaa286d
Renate <Renate@192.168.12.101>
2018-06-24 23:32:55 +0200

    asdf

---
+++ einScheiss/src/test/java/einScheiss/main/RenateTest.java
@@ -0,0 +1,12 @@
+package einScheiss.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RenateTest {
+@Test
+public void testName() throws Exception {
+	System.out.println("Ein Text");
+}
+}
