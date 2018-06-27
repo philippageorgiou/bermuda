308f1e87e3b3914c222a674bc9a60916f2463eca
Pieed <Pieed@192.168.0.59>
2018-06-27 22:53:58 +0200

    einleser ergänzt

--- einScheiss/src/main/java/bermuda/main/CsvEinleser.java
+++ einScheiss/src/main/java/bermuda/main/CsvEinleser.java
@@ -3,9 +3,12 @@ package bermuda.main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CsvEinleser {
@@ -16,4 +19,13 @@ public class CsvEinleser {
 
 	}
 
+	private List<String[]> leseVektoren(String dateiname) throws IOException {
+		FileReader fileReader = new FileReader(new File(dateiname));
+		CSVReader csvReader = new CSVReader(fileReader);
+		List<String[]> list = new ArrayList<>();
+		list = csvReader.readAll();
+		fileReader.close();
+		csvReader.close();
+		return list;
+	}
 }
